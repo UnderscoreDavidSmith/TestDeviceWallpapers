4dec396ac082fbf024c507e6a015437be0e4efa2
David Smith <david@crossforward.com>
2018-04-16 14:40:24 -0400

    Initial Commit

---
+++ TestDeviceWallpapers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestDeviceWallpapers
+//
+//  Created by David Smith on 4/16/18.
+//  Copyright © 2018 Cross Forward Consulting, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
